582ffbdb16876a1d0768c6c061915ec42b21e115
martinavagyan <martinavagyan@gmail.com>
2018-12-25 17:53:31 +0100

    fix positioning for recipes page

--- client/src/app/components/recipes/recipes.component.ts
+++ client/src/app/components/recipes/recipes.component.ts
@@ -17,11 +17,15 @@ export class RecipesComponent implements OnInit {
     slidesPerView: 1.25,
     bulletClass: 'dot',
     bulletActiveClass: 'dot-active',
+    centeredSlides: true,
   };
+
+  private numberOfSliders: number = 5;
 
   private currentSlidesPerView: number = 1;
   private screenWidth: any;
   constructor() {
+    this.config.initialSlide = Math.floor(this.numberOfSliders/2);
     this.getScreenSize();
   }
 
@@ -32,7 +36,7 @@ export class RecipesComponent implements OnInit {
   }
 
   setSlidesPerView() {
-    const tempSlidesPerview = this.screenWidth / 290;
+    const tempSlidesPerview = this.screenWidth / 240;
     if (this.currentSlidesPerView !== tempSlidesPerview) {
       this.config.slidesPerView = tempSlidesPerview;
       this.currentSlidesPerView = tempSlidesPerview;
