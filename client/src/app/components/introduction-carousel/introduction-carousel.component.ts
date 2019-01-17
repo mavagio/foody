4190567cec52ec4b0c778e56c36e9e47e766d98f
martinavagyan <martinavagyan@gmail.com>
2019-01-17 23:54:57 +0100

    test animation on mobile deploy

--- client/src/app/components/introduction-carousel/introduction-carousel.component.ts
+++ client/src/app/components/introduction-carousel/introduction-carousel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {style, state, animate, transition, trigger} from '@angular/animations';
 
 export interface Allergy {
   value: string;
@@ -13,7 +14,57 @@ enum FoodType {
 @Component({
   selector: 'app-introduction-carousel',
   templateUrl: './introduction-carousel.component.html',
-  styleUrls: ['./introduction-carousel.component.css']
+  styleUrls: ['./introduction-carousel.component.css'],
+  animations: [
+    trigger('openClose', [
+      // ...
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.5,
+        backgroundColor: 'green'
+      })),
+      transition('open => closed', [
+        animate('1s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+    ]),
+    trigger('slideInLeft', [
+        state('in', style({opacity: 1, transform: 'translate3d(0, 0, 0)'})),
+        transition('void => *', [
+          style({
+            opacity: 0,
+            transform: 'translate3d(-100%, 0, 0)'
+          }),
+          animate('400ms 200ms ease-in')
+        ])
+      ]
+    ),
+    trigger('scaleUp', [
+      state('in', style({opacity: 1, transform: 'translate3d(0, 0, 0)'})),
+        transition('void => *', [
+          style({
+            opacity: 0,
+            transform: 'scale(0.5)',
+          }),
+          animate('200ms ease-in')
+        ])
+      ]
+    ),
+    trigger('shrinkOut', [
+      state('in', style({height: '*'})),
+      transition('* => void', [
+        style({height: '*'}),
+        animate(250, style({height: 0}))
+      ])
+    ]),
+  ],
 })
 export class IntroductionCarouselComponent implements OnInit {
 
