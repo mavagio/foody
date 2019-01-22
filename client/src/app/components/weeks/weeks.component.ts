c7d697dec2bbe0135b3b495fe5837960543bff66
martinavagyan <martinavagyan@gmail.com>
2019-01-22 23:01:44 +0100

    adjusments for my weeks page

--- client/src/app/components/weeks/weeks.component.ts
+++ client/src/app/components/weeks/weeks.component.ts
@@ -13,27 +13,27 @@ export class WeeksComponent implements OnInit {
       image_url: "url(../../../assets/recipe1.jpg)"
     },
     {
-      short_description: "Mexican pulled pork ketogenic high energy taco's",
+      short_description: "Colombian abricote cottage cheese steak",
       image_url: "url(../../../assets/recipe2.jpg)"
     },
     {
-      short_description: "Mexican pulled pork ketogenic high energy taco's",
+      short_description: "Traditional cheicken with onions monstrosity",
       image_url: "url(../../../assets/recipe3.jpg)"
     },
     {
-      short_description: "Mexican pulled pork ketogenic high energy taco's",
+      short_description: "Minimal calories for your food baby maker",
       image_url: "url(../../../assets/recipe4.jpg)"
     },
     {
-      short_description: "Mexican pulled pork ketogenic high energy taco's",
+      short_description: "Eastern promises, when vodka meets potato, magic happens",
       image_url: "url(../../../assets/recipe5.jpg)"
     },
     {
-      short_description: "Mexican pulled pork ketogenic high energy taco's",
+      short_description: "Indian amnsia, you will only remember what you ate in the monring",
       image_url: "url(../../../assets/recipe6.jpg)"
     },
     {
-      short_description: "Mexican pulled pork ketogenic high energy taco's",
+      short_description: "Hand made pizza, oven napoli is jealous how crisp the crust is.",
       image_url: "url(../../../assets/recipe7.jpg)"
     },
   ]
