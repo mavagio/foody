1dd83e8fe746569bb0b91b375204c96619d5d8fb
martinavagyan <martinavagyan@gmail.com>
2019-01-23 10:52:18 +0100

    fix image loading problem

--- client/src/app/components/weeks/weeks.component.ts
+++ client/src/app/components/weeks/weeks.component.ts
@@ -10,31 +10,31 @@ export class WeeksComponent implements OnInit {
   public recipeList:Object[] = [
     {
       short_description: "Mexican pulled pork ketogenic high energy taco's",
-      image_url: "url(../../../assets/recipe1.jpg)"
+      image_url: "url(/assets/recipe1.jpg)"
     },
     {
       short_description: "Colombian abricote cottage cheese steak",
-      image_url: "url(../../../assets/recipe2.jpg)"
+      image_url: "url(/assets/recipe2.jpg)"
     },
     {
       short_description: "Traditional chicken with onions monstrosity",
-      image_url: "url(../../../assets/recipe3.jpg)"
+      image_url: "url(/assets/recipe3.jpg)"
     },
     {
       short_description: "Minimal calories for your food baby maker",
-      image_url: "url(../../../assets/recipe4.jpg)"
+      image_url: "url(/assets/recipe4.jpg)"
     },
     {
       short_description: "Eastern promises, when vodka meets potato, magic happens",
-      image_url: "url(../../../assets/recipe5.jpg)"
+      image_url: "url(/assets/recipe5.jpg)"
     },
     {
       short_description: "Indian amnsia, you will only remember what you ate in the monring",
-      image_url: "url(../../../assets/recipe6.jpg)"
+      image_url: "url(/assets/recipe6.jpg)"
     },
     {
       short_description: "Hand made pizza, oven napoli is jealous how crisp the crust is.",
-      image_url: "url(../../../assets/recipe7.jpg)"
+      image_url: "url(/assets/recipe7.jpg)"
     },
   ]
   constructor() { }
