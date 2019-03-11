5cd68ef1dbc78c29835c5861f0cede81609571f9
martinavagyan <martinavagyan@gmail.com>
2019-03-11 17:07:10 +0100

    remove js untracked files form shared folder

--- shared/consts/consts.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.USER_SETTINGS_COOKIE_NAME = "user_settings";
--- shared/models/recipeModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var NutritionCategory;
-(function (NutritionCategory) {
-    NutritionCategory[NutritionCategory["vegan"] = 0] = "vegan";
-    NutritionCategory[NutritionCategory["vegetarian"] = 1] = "vegetarian";
-    NutritionCategory[NutritionCategory["everything"] = 2] = "everything";
-    NutritionCategory[NutritionCategory["na"] = 3] = "na";
-})(NutritionCategory = exports.NutritionCategory || (exports.NutritionCategory = {}));
