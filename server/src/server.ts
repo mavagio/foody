722c3159024696f441eb6567a866dcd97dd671c7
martinavagyan <martinavagyan@gmail.com>
2019-04-08 11:31:47 +0200

    upgrade session store to mongo store

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,8 +3,9 @@ import * as mongoose from 'mongoose';
 import * as bodyParser from 'body-parser';
 import * as passport from "passport";
 import * as session from 'express-session';
-import * as cookieParser from "cookie-parser";
-import * as morgan from "morgan";
+import * as cookieParser from 'cookie-parser';
+import * as morgan from 'morgan';
+const MongoStore = require('connect-mongo')(session);
 
 const dotenv = require('dotenv').config();
 require('./config/passport')(passport);
@@ -20,6 +21,8 @@ mongoose.connect(db, {
     promiseLibrary: global.Promise,
 });
 
+const mongoStoreOption = { mongooseConnection: mongoose.connection };
+
 /**
  * Set up our express application
  * */
@@ -27,13 +30,17 @@ app.use(express.static('public'));
 app.use(express.static('files'));
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 /**
  * Set up for passport authentication
  * */
 app.use(session({
     secret: String(process.env.JWT_SECRET),
+    store: new MongoStore(mongoStoreOption),
     resave: true,
     saveUninitialized: true,
     cookie: {
